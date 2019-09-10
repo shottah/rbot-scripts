f1d60ea8119f83b01112a591b94cec80d8218811
Matthew Abraham <wasteofintel@gmail.com>
2019-09-10 10:12:47 +0100

    bone dust (max)

---
+++ bone_dust.cs
@@ -0,0 +1,55 @@
+/*
+	Created by matabeitt
+	This script farms for Bone Dust (max) from 
+	Battleunder B.
+
+	Requirements:
+		The player must have 1 (one) free inventory 
+		space.	
+
+	Notes:
+		The script uses a private room.
+*/
+
+using RBot;
+using System;
+
+public class Script{
+
+	public void ScriptMain(ScriptInterface bot){
+	
+		bot.Skills.StartTimer();
+		bot.Options.SafeTimings = true;
+		bot.Options.RestPackets = true;
+		bot.Options.PrivateRooms = true;
+		bot.Options.ExitCombatBeforeQuest = true;
+		
+		for (int i = 1; i <= 4; i++) {
+			bot.Skills.Add(i, 1f);
+		}
+		
+		
+		bot.Player.Join("battleunderb");
+		string item = "Bone Dust";
+		int q = 5100;
+		
+		bot.Bank.ToInventory(item);
+		bot.Drops.Add(item);
+		bot.Drops.Start();
+		
+		double hp;
+		while (!bot.ShouldExit() || bot.Inventory.Contains(item, quantity:q)) {
+			hp = (double) bot.Player.Health / bot.Player.MaxHealth;
+			bot.Log(hp.ToString());
+			if (hp < 0.4) {
+				bot.Player.Jump("r1", "Left");
+				bot.Player.Rest(full:true);
+				bot.Player.Jump("Enter", "Right");
+			}
+			else bot.Player.Hunt("Skeleton Warrior|Skeleton Fighter");
+		}
+		
+		bot.Drops.Stop();
+		bot.Exit();
+	}
+}
