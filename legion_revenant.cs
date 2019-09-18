4ce3fa445dd3685525b4cdf4c85b721b275c96b7
Matthew Abraham <wasteofintel@gmail.com>
2019-09-18 12:04:57 +0100

    change map

--- legion_revenant.cs
+++ legion_revenant.cs
@@ -86,8 +86,8 @@ public class Script {
 		if (bot.Map.Name != "revenant") bot.Player.Join("revenant");
 		FarmRoutine(bot, "revenant", "r2", "Left", "Forgotten Soul", "Tethered Soul", 300);
 		
-		if (bot.Map.Name != "revenant") bot.Player.Join("revenant");
-		FarmRoutine(bot, "revenant", "r3", "Left", "Ultra Aeacus", "Aeacus Empowered", 50);
+		if (bot.Map.Name != "judgement") bot.Player.Join("judgement");
+		FarmRoutine(bot, "judgement", "r10a", "Down", "Ultra Aeacus", "Aeacus Empowered", 50);
 		
 		if (bot.Map.Name != "shadowrealm") bot.Player.Join("shadowrealm");
 		FarmRoutine(bot, "shadowrealm", "Enter", "Spawn", "*", "Darkened Essence", 500);
